4ec693df8151b468989c72d1cf74deb02dc1fa0c
Chris Dent <chdent@redhat.com>
2014-12-12 16:51:27 +0000

    Get rid of testresources, too much complexity
    There's no point managing a complex graph. Instead just keep
    track of the linear sequence of dependencies.
    
    If a test has been run in this process, don't run it again.
    
    Some jiggery pokery is required to make sure that setUp and tearDown
    are handled appropriately.
    
    Group tests (for concurrency) on the name of the yaml file.
    
    Thanks to @FND for some rubber ducking to get this figured out. He
    helped me clear away the dismal hate for testr, subunit, etc. I
    still hate them but at least they aren't in the way anymore.

--- gabbi/driver.py
+++ gabbi/driver.py
@@ -2,8 +2,10 @@
 import glob
 import os
 
-import testresources
+import testtools
 import yaml
+
+from unittest import suite
 
 
 def load_yaml(yaml_file):
@@ -17,33 +19,32 @@ class Builder(type):
         return type.__new__(cls, name, bases, d)
 
 
-class HTTPTestCase(testresources.ResourcedTestCase):
+class HTTPTestCase(testtools.TestCase):
+
+    def setUp(self):
+        if not self.has_run:
+            super(HTTPTestCase, self).setUp()
+
+    def tearDown(self):
+        if not self.has_run:
+            super(HTTPTestCase, self).tearDown()
+        self.has_run = True
 
     def runTest(self):
+        if self.has_run:
+            return
+        if self.prior and not self.prior.has_run:
+            self.prior.run()
         self.assertTrue(self.test_data['url'])
 
 
-class PriorTest(testresources.TestResourceManager):
-
-    def __init__(self, suite, prior):
-        self.suite = suite
-        self.prior = prior
-        super(PriorTest, self).__init__()
-
-    def make(self, dependency):
-        self.prior.run(testresources._get_result())
-
-    def isDirty(self):
-        return False
-
-
 def build_tests(path, loader, tests, pattern):
-    top_suite = testresources.OptimisingTestSuite()
+    top_suite = suite.TestSuite()
 
     path = '%s/*.yaml' % path
 
     for test_file in glob.iglob(path):
-        file_suite = testresources.OptimisingTestSuite()
+        file_suite = suite.TestSuite()
         test_data = load_yaml(test_file)
         test_base_name = os.path.splitext(os.path.basename(test_file))[0]
         prior_test = None
@@ -51,10 +52,9 @@ def build_tests(path, loader, tests, pattern):
             test_name = '%s_%s' % (test_base_name,
                                    test['name'].lower().replace(' ', '_'))
             klass = Builder(test_name, (HTTPTestCase,),
-                            {'test_data': test})
-            if prior_test:
-                klass.resources = [('prior',
-                                    PriorTest(file_suite, prior_test))]
+                            {'test_data': test,
+                             'prior': prior_test,
+                             'has_run': False})
             tests = loader.loadTestsFromTestCase(klass)
             this_test = tests._tests[0]
             file_suite.addTest(this_test)
