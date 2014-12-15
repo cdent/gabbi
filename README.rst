d5583d1fde9e5fb904dc03bd36ac66b4a7254166
Chris Dent <chdent@redhat.com>
2014-12-15 13:04:08 +0000

    Add some docstrings and a bit more README

--- README.rst
+++ README.rst
@@ -4,5 +4,43 @@ gabbi
 Gabbi is a tool for running HTTP tests where requests and responses
 are represented in a declarative form.
 
-_This is just an experiment, do not expect it to work or even
-pretend to do so._
+It is currently under development and does not yet work (it will
+create TestSuites from YAML files, but not yet execute any http
+requests).
+
+Purpose
+-------
+
+Gabbi works to bridge the gap between human readable YAML files that
+represent HTTP requests and expected responses and the obscured realm of
+Python-based, object-oriented unit tests in the style of the unittest
+module and its derivatives.
+
+Each YAML file represents an order list of HTTP requests along with
+the expected responses. This allows a single file to represent a
+process in the API, not just a single request in isolation. For
+example:
+
+* Create a resource.
+* Retrieve a resource.
+* Delete a resource.
+* Retrieve a resource again to confirm it is gone.
+
+At the same time it is still possible to ask gabbi to run just one
+request. If it is in a sequence of tests, those tests prior to it in
+the YAML file will be run (in order). In any single process any test
+will only be run once. Concurrency is handled such that one file
+runs in one process.
+
+The features mean that it is possible to create tests that are
+useful for both humans (as tools for improving and developing APIs)
+and automated CI systems.
+
+To Do
+-----
+
+* [ ] Do the actual HTTP requesting and response evaluation.
+* [ ] Enable optional use of wsgi-intercept so no web server is
+      required.
+* [ ] Consider fixture management (data store) per YAML file.
+* [ ] Dealing with driving authN and authZ.
--- gabbi/driver.py
+++ gabbi/driver.py
@@ -1,25 +1,62 @@
+# Copyright 2014 Red Hat
+#
+# Authors: Chris Dent <chdent@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Generate HTTP tests from YAML files
+
+Each HTTP request is its own TestCase and can be requested to be run in
+isolation from other tests. If it is member of a sequence of requests,
+prior requests will be run.
+
+A sequence is represented by an ordered list in a single YAML file.
+
+Each sequence becomes a TestSuite.
+
+An entire directory of YAML files is a TestSuite of TestSuites.
+"""
 
 import glob
 import os
+from unittest import suite
 
 import testtools
 import yaml
 
-from unittest import suite
-
 
 def load_yaml(yaml_file):
+    """Read and parse any YAML file. Let exceptions flow where they may.
+    """
     with open(yaml_file) as source:
         return yaml.safe_load(source.read())
 
 
 class Builder(type):
 
-    def __new__(cls, name, bases, d):
-        return type.__new__(cls, name, bases, d)
+    def __new__(mcs, name, bases, d):
+        return type.__new__(mcs, name, bases, d)
 
 
 class HTTPTestCase(testtools.TestCase):
+    """Encapsulate a single HTTP request as a TestCase.
+
+    If the test is a member of a sequence of requests, ensure that prior
+    tests are run.
+
+    To keep the test harness happy we need to make sure the setUp and
+    tearDown are only run once.
+    """
 
     def setUp(self):
         if not self.has_run:
@@ -30,7 +67,7 @@ class HTTPTestCase(testtools.TestCase):
             super(HTTPTestCase, self).tearDown()
         self.has_run = True
 
-    def runTest(self):
+    def test_request(self):
         if self.has_run:
             return
         if self.prior and not self.prior.has_run:
@@ -39,6 +76,10 @@ class HTTPTestCase(testtools.TestCase):
 
 
 def build_tests(path, loader, tests, pattern):
+    """Read YAML files from a directory to create tests.
+
+    Each YAML file represents an ordered sequence of HTTP requests.
+    """
     top_suite = suite.TestSuite()
 
     path = '%s/*.yaml' % path
@@ -47,18 +88,23 @@ def build_tests(path, loader, tests, pattern):
         file_suite = suite.TestSuite()
         test_data = load_yaml(test_file)
         test_base_name = os.path.splitext(os.path.basename(test_file))[0]
+
         prior_test = None
         for test in test_data:
             test_name = '%s_%s' % (test_base_name,
                                    test['name'].lower().replace(' ', '_'))
+            # Use metaclasses to build a class of the necessary type
+            # with relevant arguments.
             klass = Builder(test_name, (HTTPTestCase,),
                             {'test_data': test,
                              'prior': prior_test,
                              'has_run': False})
+
             tests = loader.loadTestsFromTestCase(klass)
             this_test = tests._tests[0]
             file_suite.addTest(this_test)
             prior_test = this_test
+
         top_suite.addTest(file_suite)
 
     return top_suite
