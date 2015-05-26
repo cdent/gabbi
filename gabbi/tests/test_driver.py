6bc26cb880871e3707f38cfeacb0dcdfdda8c234
Chris Dent <chris.dent@gmail.com>
2015-05-26 23:39:43 +0100

    Initial tests for build_tests

---
+++ gabbi/tests/test_driver.py
@@ -0,0 +1,43 @@
+#
+# Copyright 2014, 2015 Red Hat. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Test that the driver can build tests effectively.
+"""
+
+import os
+import testtools
+import unittest
+
+from gabbi import driver
+
+TESTS_DIR = 'test_gabbits'
+
+
+class DriverTest(testtools.TestCase):
+
+    def setUp(self):
+        super(DriverTest, self).setUp()
+        self.loader = unittest.defaultTestLoader
+        self.test_dir = os.path.join(os.path.dirname(__file__), TESTS_DIR)
+
+    def test_driver_loads_one_test(self):
+        suite = driver.build_tests(self.test_dir, self.loader,
+                                   host='localhost', port=8001)
+        self.assertEqual(1, len(suite._tests),
+                         'top level suite contains one suite')
+        self.assertEqual(1, len(suite._tests[0]._tests),
+                         'contained suite contains one test')
+        self.assertEqual('test_driver_single_one',
+                         suite._tests[0]._tests[0].__class__.__name__,
+                         'test class name maps')
