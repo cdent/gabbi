1f0b1f7542c0e3149945ad41c51d1b1b7653af3f
Chris Dent <chris.dent@gmail.com>
2015-07-20 13:57:05 +0100

    Be ambiguous about copyright
    The part that matters is the license not who or what holds it.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Gabbi'
-copyright = u'2014-2015, Chris Dent'
+copyright = u''
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -168,7 +168,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
