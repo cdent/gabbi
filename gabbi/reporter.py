6db275045c90e195d9b56fa73893840549f0616d
Chris Dent <chris.dent@gmail.com>
2015-07-30 11:01:04 +0100

    Make the pep8 gods happy
    traceback had been imported to experiment with formatting the
    traceback (on err[2]) to see if there were ways to use it in the
    output production. It probably can be but likely some form of flag
    will be desired to turn it on or off.

--- gabbi/reporter.py
+++ gabbi/reporter.py
@@ -12,7 +12,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import traceback
 from unittest import TextTestResult
 from unittest import TextTestRunner
 
