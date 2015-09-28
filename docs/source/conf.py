aa028c5bc1cca8f73b61e9e9e044786157e9185a
Chris Dent <chris.dent@gmail.com>
2015-09-28 17:06:56 +0100

    hack to override rtd back table formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,6 +103,12 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+html_context = {'css_files': [
+    'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+    'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+    '_static/theme_overrides.css',
+]}
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
