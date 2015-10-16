58c425846c0794ba348e3eff23c45c1ae1c4dce7
FND <fnd@localhost.localdomain>
2015-10-16 09:09:40 +0200

    Fix whitespace for consistency
    I'd actually prefer having a blank line between license and docstring,
    but this is how most modules do it

--- gabbi/driver.py
+++ gabbi/driver.py
@@ -10,7 +10,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
 """Generate HTTP tests from YAML files
 
 Each HTTP request is its own TestCase and can be requested to be run in
