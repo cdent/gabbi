438dff4eb4ee79275c0acd6d08407aec473c5b0f
Chris Dent <cdent@anticdent.org>
2016-03-10 15:22:49 +0000

    Switch to using jsonpath_rw_ext 1.0.0
    Version 1.0.0 merges the non-monkeypatch version of the changes
    which this patch removes from gabbi.

--- gabbi/json_parser.py
+++ gabbi/json_parser.py
@@ -10,35 +10,12 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-"""Extend jsonpath_rw to add a len command."""
+"""Keep one single global jsonpath parser."""
 
-import functools
-
-import jsonpath_rw
-from jsonpath_rw_ext import _iterable
 from jsonpath_rw_ext import parser
 
 
 PARSER = None
-
-
-def custom_find(self, datum):
-    """Return sorted value of This if list or dict."""
-    if isinstance(datum.value, dict) and self.expressions:
-        return datum
-
-    if isinstance(datum.value, dict) or isinstance(datum.value, list):
-        key = (functools.cmp_to_key(self._compare)
-               if self.expressions else None)
-        return [jsonpath_rw.DatumInContext.wrap(
-            [value for value in sorted(datum.value, key=key)])]
-    return datum
-
-
-# Override jsonpath_pw_ext return from find so we have the right
-# data structure. Monkey patch because there are too many places to
-# change in the lexer and parser.
-_iterable.SortedThis.find = custom_find
 
 
 def parse(path):
